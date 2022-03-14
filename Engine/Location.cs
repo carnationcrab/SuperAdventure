c359d12039db6b49209dfe22420ec0ba188b3eb4
samma <samma@LAPTOP-CM9SGJ57>
2022-03-13 19:28:34 -0500

    Add project files.

---
+++ Engine/Location.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine
+{
+    public class Location
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
